91d20384194317fe4780af4a7282a1c8ac904c7f
Sam Bebenek <sam.bebenek@gmail.com>
2020-06-11 20:43:24 -0400

    fix portfolio link bug

--- src/components/body-portfolio-pieces.js
+++ src/components/body-portfolio-pieces.js
@@ -23,7 +23,7 @@ export default class BodyPortfolioPieces extends React.Component {
         for (let element of undrawnImages) {
             animationString = "animation: portfolioAppear 0.5s ease-out " + delayCounter + "s forwards 1;";
             element.setAttribute("style", animationString);
-            delayCounter += 0.3; //the rate at which each image is drawn
+            delayCounter += 0; //the rate at which each image is drawn
         }
     }
     componentDidMount() {
@@ -44,9 +44,9 @@ export default class BodyPortfolioPieces extends React.Component {
                 </div>
                 <div className="portfolio-right">
                     <div className="slider-holder">
-                        <div className="slider portfolio-card-holder" data-slick='{"slidesToShow": 1, "slidesToScroll": 1, "accessibility": true}' projectlink="/myspotify">
+                        <div className="slider portfolio-card-holder" data-slick='{"slidesToShow": 1, "slidesToScroll": 1, "accessibility": true}'>
                             <PortfolioCard title="MySpotify" techUsed="PHP, JavaScript, jQuery, Spotify API, Seatgeek API"
-                                imageSrc="screenshots/myspotify.png" projectLink="/myspotify" description="MySpotify is a web app anyone with a Spotify account can log 
+                                imageSrc="screenshots/myspotify.png" projectlink="/myspotify" description="MySpotify is a web app anyone with a Spotify account can log 
                                 into to see their top artists. Using the Spotify API, users can pick from a set of timeframes used to calculate their top artists, and can see who 
                                 they’ve been listening to most recently, or over the lifetime of their account. Additionally, the Seatgeek API is used so that users can click on their 
                                 favorite artists to see a list of that artist or group’s upcoming shows, along with a link to buy tickets.
@@ -57,10 +57,10 @@ export default class BodyPortfolioPieces extends React.Component {
                                 able to find workout partners in their area. FitnessTracker was designed for fitness enthusiasts of all kinds - from serious bodybuilders to 
                                 those just thinking about going on their first run. This app was made for anyone who wanted help achieving their fitness dreams." />
                             <CaseStudyCard title="GearLog" techUsed="C#, .NET, MS Entity Framework" />
-                            <PortfolioCard title="Dog Website" techUsed="React, Express, MySQL" imageSrc="screenshots/example.jpg" projectlink="#"
+                            <PortfolioCard title="Dog Website" techUsed="React, Express, MySQL" imageSrc="screenshots/dogwebsite.png" projectlink="#"
                                 description="Dog Website is a content management system designed to connect dog shelters and fosterers in the GTA with potential adopters. 
                                 Users are able to see a list of dogs up for adoption. Administrators are able to dynamically change the content of the website. This app features a 
-                                front-end built with React, a back end RESTful API server built with Express, and a MySQL database." />
+                                front-end built with React, a back end RESTful API server built with Express, and a MySQL database." isLive="false" />
                             <PortfolioCard title="Five Card Draw" techUsed="HTML, CSS, jQuery" imageSrc="screenshots/fivecarddraw.png" projectlink="/fivecarddraw"
                                 description="Five Card Draw is a web app where you can play the card game of the same name. Try to get as many suit combinations as possible! 
                                 All animations are made with CSS." />
--- src/components/portfolio-card.js
+++ src/components/portfolio-card.js
@@ -4,14 +4,21 @@ import React from 'react';
  * This class holds the card containing the portfolio item screenshot, description, and link to the hosted version
  */
 export default class ProtfolioCard extends React.Component {
+
     render() {
+        let liveButton = <a href={this.props.projectlink}><button className="btn btn-outline-light">See Live Demo</button></a>;
+        //if the isLive attribute is set to false, don't print a link button
+        if (Boolean(this.props.isLive) !== false)
+        {
+            liveButton = <p>Live demo coming soon!</p>
+        }
         return (
             <div className="portfolio-card">
                 <h1 className="mb-3">{this.props.title}</h1>
                 <img className="portfolio-screenshot mb-3" src={this.props.imageSrc} alt="website screenshot" />
                 <p><em>Technologies Used: {this.props.techUsed}</em></p>
                 <p>{this.props.description}</p>
-                <a href={this.props.projectlink}><button className="btn btn-outline-light">See Live Demo</button></a>
+                {liveButton}
             </div>
         );
     }
